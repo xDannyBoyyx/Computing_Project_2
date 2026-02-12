8d964a6f063ab57427057d497692983389dfba76
xDannyBoyyx <voytdan0704@gmail.com>
2026-02-12 12:12:03 +0000

    changed few things including adding files and images and creating the base backgrould / map layer for the game. Simple configs using phaser and styling applied

---
+++ game.js
@@ -0,0 +1,54 @@
+// Where the game will work together
+
+// Configuration for the game and how it works
+const config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      // gravity: { y: 0 },
+      debug: false //could change to true when debugging
+    }
+  },
+
+  render: {
+    pixelArt: true
+  },
+
+  scale: {
+    mode: Phaser.Scale.FIT, // May change to fit better
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+// Creating it via Phaser
+const game = new Phaser.Game(config);   
+
+// Preloading images
+function preload() {
+  this.load.tilemapTiledJSON('mapJson', 'map.json');
+  this.load.image('grassTiles', 'assets/GRASS+.png');
+}   
+
+// Creating features including the map and assets
+function create() {
+  const map = this.make.tilemap({ key: 'mapJson' });
+
+  const tileset = map.addTilesetImage('grass', 'grassTiles');
+
+  map.createLayer('Background', tileset, 0, 0); 
+} 
+
+// Updating features and their technicality
+function update() {
+
+}
