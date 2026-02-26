a604d19d70ba0b887f1c7e99ccda2a31caed58f8
Arny69 <abudr001@campus.goldsmiths.ac.uk>
2026-02-26 14:34:20 +0000

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -12,21 +12,21 @@ export class MainMenu extends Phaser.Scene {
         this.add.image(320, 180, 'menuScreen');
         
         // START button (adjust x, y, width, height to match the image)
-        let startButton = this.add.rectangle(320, 190, 170, 40, 0x000000, 0.5); // 0 alpha = invisible
+        let startButton = this.add.rectangle(320, 190, 165, 40, 0x000000, 0.5); // 0 alpha = invisible
         startButton.setInteractive({ useHandCursor: true });
         startButton.on('pointerdown', () => {
             this.scene.start('GameScene');
         });
         
         // OPTIONS button
-        let optionsButton = this.add.rectangle(320, 240, 170, 40, 0x000000, 0.5);
+        let optionsButton = this.add.rectangle(320, 235, 165, 40, 0x000000, 0.5);
         optionsButton.setInteractive({ useHandCursor: true });
         optionsButton.on('pointerdown', () => {
             console.log('Options clicked');
         });
         
         // EXIT button
-        let exitButton = this.add.rectangle(320, 290, 170, 40, 0x000000, 0.5);
+        let exitButton = this.add.rectangle(320, 280, 165, 40, 0x000000, 0.5);
         exitButton.setInteractive({ useHandCursor: true });
         exitButton.on('pointerdown', () => {
             window.close();
