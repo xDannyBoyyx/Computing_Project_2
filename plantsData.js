10a29fb2c0f0c2543b7160cfa2dba4df78baeb81
Arny69 <abudr001@campus.goldsmiths.ac.uk>
2026-03-12 13:33:17 +0000

    added comment for frameNumber change

--- plantsData.js
+++ plantsData.js
@@ -26,6 +26,6 @@ export const plantData = {
         harvestMin: 2,
         harvestMax: 5,
         spriteSheet: "largePlant", 
-        startFrame: 0
+        startFrame: 1 //TO BE CHANGED TO CORRECT FRAME NUMBER
     }
 }
