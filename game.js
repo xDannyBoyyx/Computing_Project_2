5fae9382a25112f2dafc7cd0d258a433cb122bfb
Arny69 <abudr001@campus.goldsmiths.ac.uk>
2026-03-15 19:35:07 +0000

    Update game.js

--- game.js
+++ game.js
@@ -98,7 +98,7 @@ class GameScene extends Phaser.Scene {
     });
     
     this.player = new Player(this, 320, 200, selectedGender); // Pass gender to player
-    this.cameras.main.startFollow(this.player.sprite);
+    this.cameras.main.startFollow(this.player.sprite, false, 0.1, 0.1);
     this.cameras.main.setDeadzone(100, 50);
     this.cameras.main.setBounds(0, 0, 40 * 16, 25 * 16);
     this.physics.world.setBounds(0, 0, 40 * 16, 25 * 16);
