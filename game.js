d6df431f0689723c72c8913a43e56c6831f267e4
Arny69 <abudr001@campus.goldsmiths.ac.uk>
2026-03-15 19:37:19 +0000

    Update game.js

--- game.js
+++ game.js
@@ -101,7 +101,7 @@ class GameScene extends Phaser.Scene {
     this.cameras.main.startFollow(this.player.sprite, false, 0.1, 0.1);
     this.cameras.main.setDeadzone(100, 50);
     this.cameras.main.setBounds(0, 0, 40 * 16, 25 * 16);
-    this.physics.world.setBounds(0, 0, 40 * 16, 25 * 16);
+    // this.physics.world.setBounds(0, 0, 40 * 16, 25 * 16);
     
     this.player.sprite.setCollideWorldBounds(true);
     this.hotbar = new Hotbar(this);
