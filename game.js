d19069cb381ca540655de2b9effc4920d98d3823
Arny69 <abudr001@campus.goldsmiths.ac.uk>
2026-03-25 18:53:54 +0000

    added graphPouch image

--- game.js
+++ game.js
@@ -49,6 +49,7 @@ class GameScene extends Phaser.Scene {
     this.load.image('carrotPouch', 'assets/pouches/carrotPouch.png');
     this.load.image('raddishPouch', 'assets/pouches/raddishPouch.png');
     this.load.image('cabbagePouch', 'assets/pouches/cabbagePouch.png');
+    this.load.image('grapePouch', 'assets/pouches/cabbagePouch.png');
     
     // Spritesheets VVV
 
